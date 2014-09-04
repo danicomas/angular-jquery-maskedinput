7858736d682a261432eab6bf8c4b3c036c6d26c2
danicomas <danicomas@gmail.com>
2014-09-04 13:46:39 +0200

    Added JavaScript

---
+++ angular-jquery-maskedinput.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+angular.module('angular-jquery-maskedinput', []).directive('mask', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attr, ctrl) {
+
+            if (attr.mask)
+                elem.mask(attr.mask, { placeholder: attr.maskPlaceholder });
+        }
+    };
+});
